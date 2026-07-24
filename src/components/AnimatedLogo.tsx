28769884f7425095d746b25d95289f239f765bb8
Nico Recio <nico@anitaseyes.com>
2026-07-24 07:19:53 +0000

    Hero mobile: logo más arriba y contenido sube

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -18,7 +18,7 @@ export function AnimatedLogo() {
       className={`fixed z-50 w-auto pointer-events-none transition-all duration-700 ease-in-out ${
         scrolled
           ? "top-4 left-6 h-12 md:top-2 md:h-20"
-          : "top-16 left-1/2 -translate-x-1/2 h-32 md:top-28 md:h-72"
+          : "top-20 left-1/2 -translate-x-1/2 h-28 md:top-28 md:h-72"
       }`}
     />
   );
