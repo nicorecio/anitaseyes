f1eaba070fa2a9b1e6fea40967a3f1bb8ff6aa8a
Nico Recio <nico@anitaseyes.com>
2026-07-24 09:14:09 +0000

    Logo clickable para volver al inicio al hacer scroll

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -11,14 +11,19 @@ export function AnimatedLogo() {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <img
       src={logoAsset.url}
       alt="Anita's Eyes"
-      className={`fixed z-50 w-auto pointer-events-none transition-all duration-700 ease-in-out ${
+      onClick={handleClick}
+      className={`fixed z-50 w-auto transition-all duration-700 ease-in-out ${
         scrolled
-          ? "top-4 left-6 h-12 md:top-2 md:h-20"
-          : "top-20 left-1/2 -translate-x-1/2 h-24 md:top-24 md:h-72"
+          ? "top-4 left-6 h-12 md:top-2 md:h-20 cursor-pointer"
+          : "top-20 left-1/2 -translate-x-1/2 h-24 md:top-24 md:h-72 pointer-events-none"
       }`}
     />
   );
